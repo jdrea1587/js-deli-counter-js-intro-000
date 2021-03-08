6d060a5e36696abdd863931198fb13b70ae22c9e
Joshua Rea <joshdanielrea@gmail.com>
2021-03-08 18:43:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function nowServing(katzDeliLine){
     i++;
   } if (katzDeliLine.length === 0){
     return "There is nobody waiting to be served!";
-  }else return (`Currently serving ${katzDeliLine.shift()}`);
+  }else return (`Currently serving ${katzDeliLine.shift().}`);
 }
